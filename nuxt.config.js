8ac8dc904bc5c7f83c367f446ea8fa6b8a3cfa49
Fatoumata <fahtima8778@gmail.com>
2019-04-02 14:48:09 +0800

    added progress bar

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,74 @@
-require('dotenv').config();
-const contentful = require('contentful');
+require("dotenv").config();
+const contentful = require("contentful");
 
 module.exports = {
   /*
    ** Headers of the page
    */
   head: {
-    title: 'kidocode blog',
-    meta: [{
-        charset: 'utf-8'
+    title: "kidocode blog",
+    meta: [
+      {
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Read about science and technology from the youngest in the field coached by expert in the field.'
+        hid: "description",
+        name: "description",
+        content:
+          "Read about science and technology from the youngest in the field coached by expert in the field."
       }
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico',
-    }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      }
+    ]
   },
   /*
    ** Customize the progress bar color
    */
   loading: {
-    color: '#fff'
+    color: "green",
+    height: "4px"
   },
-  modules: ['@nuxtjs/dotenv', '@nuxtjs/markdownit', '@nuxtjs/bulma', '@nuxtjs/font-awesome', ['@nuxtjs/pwa', {
-    icon: false
-  }]],
+  modules: [
+    "@nuxtjs/dotenv",
+    "@nuxtjs/markdownit",
+    "@nuxtjs/bulma",
+    "@nuxtjs/font-awesome",
+    [
+      "@nuxtjs/pwa",
+      {
+        icon: false
+      }
+    ]
+  ],
   markdownit: {
     injected: true
   },
 
-
   /*
    ** Global CSS
    */
-  css: ['~/assets/style.css'],
+  css: ["~/assets/style.css"],
   /*
    ** Build configuration
    */
-  plugins: [{
-    src: '~plugins/social.js'
-  }, ],
+  plugins: [
+    {
+      src: "~plugins/social.js"
+    }
+  ],
   build: {
     postcss: {
       plugins: {
-        'postcss-preset-env': {
+        "postcss-preset-env": {
           features: {
             customProperties: false
           }
@@ -64,35 +79,34 @@ module.exports = {
      ** Run ESLint on save
      */
     manifest: {
-      name: 'Myknuxt',
-      short_name: 'knuxtjs',
-      icons: [{
-        "src": "static/kidocode_logo.png",
-        "sizes": "192x192",
-        "type": "image/png"
-      }]
+      name: "Myknuxt",
+      short_name: "knuxtjs",
+      icons: [
+        {
+          src: "static/kidocode_logo.png",
+          sizes: "192x192",
+          type: "image/png"
+        }
+      ]
     },
-    extend(config, {
-      isDev,
-      isClient
-    }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
         });
         config.module.rules.push({
           test: /(\.vue|\.js)$/,
-          loader: 'babel-loader',
+          loader: "babel-loader",
           exclude: /node_modules/,
           exclude: /.nuxt/
         });
       }
       config.node = {
-        fs: 'empty'
-      }
+        fs: "empty"
+      };
     }
   },
   generate: {
@@ -102,19 +116,21 @@ module.exports = {
         accessToken: process.env.CTF_CD_ACCESS_TOKEN
       });
 
-      return client.getEntries({
-        content_type: 'blogPost',
+      return client
+        .getEntries({
+          content_type: "blogPost",
 
-        'fields.tags[in]': 'entry.fields.tags'
-      }).then((response) => {
-        return response.items.map(entry => {
-          return {
-            route: entry.fields.slug,
-            imageURL: 'https:' + entry.fields.file.fields.file.url,
-            payload: entry
-          };
+          "fields.tags[in]": "entry.fields.tags"
+        })
+        .then(response => {
+          return response.items.map(entry => {
+            return {
+              route: entry.fields.slug,
+              imageURL: "https:" + entry.fields.file.fields.file.url,
+              payload: entry
+            };
+          });
         });
-      });
     }
   }
-}
+};
